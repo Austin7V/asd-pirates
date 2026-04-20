dc6489b2a6e702824cca0d3ca31685a839afce89
Sergey Badin <sergeykfg1@me.com>
2026-04-20 12:15:43 +0200

    feat: select and type form and list DOM elements

--- Typescript/ts_client_side/book-search/dist/main.js
+++ Typescript/ts_client_side/book-search/dist/main.js
@@ -1,3 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const searchForm = document.getElementById('search-form');
+const bookSearch = document.getElementById('book-search');
+console.log(bookSearch);
+console.log(searchForm);
 //# sourceMappingURL=main.js.map
---
+++ Typescript/ts_client_side/book-search/src/main.ts
@@ -0,0 +1,5 @@
+const searchForm = document.getElementById('search-form') as HTMLInputElement;
+const bookSearch = document.getElementById('book-search') as HTMLInputElement;
+
+console.log(bookSearch);
+console.log(searchForm);
