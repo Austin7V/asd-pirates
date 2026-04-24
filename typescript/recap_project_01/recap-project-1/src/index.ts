2425798e344e690142c6946f2bdf8a196afa0543
Sergey Badin <sergeykfg1@me.com>
2026-04-24 11:38:02 +0200

    add fetch

--- typescript/recap_project_01/recap-project-1/src/index.ts
+++ typescript/recap_project_01/recap-project-1/src/index.ts
@@ -1,1 +1,22 @@
-console.log("hello");
+type Book = {
+    id: string;
+    title: string;
+    subtitle: string;
+    isbn: string;
+    abstract: string;
+    author: string;
+    publisher: string;
+    price: string;
+    numPages: number;
+    cover: string;
+    userId: number;
+}
+
+const API_URL = "http://localhost:4730/books";
+
+async function loadBooks() {
+    const response = await fetch(API_URL);
+    const books: Book[] = await response.json();
+    console.log(books);
+}
+loadBooks();
