d90020ac9d4345491c2343d1fdb7fe2be5b15708
Sergey Badin <sergeykfg1@me.com>
2026-04-24 12:15:17 +0200

    feat: render books

--- typescript/recap_project_01/recap-project-1/src/index.ts
+++ typescript/recap_project_01/recap-project-1/src/index.ts
@@ -13,10 +13,58 @@ type Book = {
 }
 
 const API_URL = "http://localhost:4730/books";
+const bookList = document.querySelector<HTMLTableSectionElement>("#book-list");
 
 async function loadBooks() {
     const response = await fetch(API_URL);
     const books: Book[] = await response.json();
     console.log(books);
+    renderBooks(books);
 }
-loadBooks();
+
+function renderBooks(books: Book[]) {
+    if (!bookList) {
+        return;
+    }
+
+    for (const book of books) {
+        const row = document.createElement("tr");
+
+        const favoriteCell = document.createElement("td");
+        const favoriteButton = document.createElement("button");
+        favoriteButton.className = "button button-clear fav-btn";
+        favoriteButton.textContent = "";
+        favoriteCell.append(favoriteButton);
+
+        const titleCell = document.createElement("td");
+        titleCell.textContent = book.title;
+
+        const isbnCell = document.createElement("td");
+        isbnCell.textContent = book.isbn;
+
+        const authorCell = document.createElement("td");
+        authorCell.textContent = book.author;
+
+        const publisherCell = document.createElement("td");
+        publisherCell.textContent = book.publisher;
+
+        const detailCell = document.createElement("td");
+        const detailButton = document.createElement("button");
+        detailButton.className = "button";
+        detailButton.textContent = "Detail";
+        detailCell.append(detailButton);
+
+        row.append(
+            favoriteCell,
+            titleCell,
+            isbnCell,
+            authorCell,
+            publisherCell,
+            detailCell
+        );
+
+        bookList.append(row);
+    }
+}
+
+loadBooks()
