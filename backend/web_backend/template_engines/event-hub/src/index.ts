aafaded800fbe9d6622211cd7762a8ed15c272b8
Sergey Badin <sergeykfg1@me.com>
2026-04-28 17:00:31 +0200

    feat challenges json post

--- backend/web_backend/template_engines/event-hub/src/index.ts
+++ backend/web_backend/template_engines/event-hub/src/index.ts
@@ -1,8 +1,30 @@
-import express from "express";
+import express, {request, response} from "express";
 import nunjucks from "nunjucks";
+import posts from "./data/posts.json"
 
 const app = express();
 const port = 3000;
+
+const events = [
+    {
+        name: "React Conf",
+        date: "June 10, 2025",
+        location: "Berlin",
+        soldOut: false,
+    },
+    {
+        name: "Vue.js Summit",
+        date: "July 2, 2025",
+        location: "Amsterdam",
+        soldOut: true,
+    },
+    {
+        name: "Node.js Interactive",
+        date: "August 15, 2025",
+        location: "London",
+        soldOut: false,
+    },
+];
 
 nunjucks.configure("views", {
     autoescape: true,
@@ -13,6 +35,16 @@ nunjucks.configure("views", {
 
 app.get("/", (req, res) => {
     res.render("index.html");
+});
+
+app.get("/events", (request, response) => {
+    response.render("events.html", {
+        events,
+    });
+});
+
+app.get("/posts", (req, res)=> {
+    res.json("posts");
 });
 
 app.listen(port, () => {
