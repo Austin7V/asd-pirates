a401d3b5ecfe7b1b4f0f65d2a7e0d0473d5569de
Sergey Badin <sergeykfg1@me.com>
2026-05-05 17:01:29 +0200

    Add GET bookmarks endpoint

--- backend/bookmark/src/index.ts
+++ backend/bookmark/src/index.ts
@@ -1,15 +1,37 @@
 import express from "express";
 
+interface Bookmark {
+    id: number;
+    url: string;
+    title: string;
+    tag?: string;
+}
+
 const app = express();
 
 const port = 3000;
 
 app.use(express.json());
 
+let bookmarks: Bookmark[] = [
+    { id: 1, url: "https://expressjs.com", title: "Express.js", tag: "node" },
+    {
+        id: 2,
+        url: "https://typescriptlang.org",
+        title: "TypeScript",
+        tag: "typescript",
+    },
+    { id: 3, url: "https://developer.mozilla.org", title: "MDN Web Docs" },
+];
+
 app.get("/", (request, response) => {
     response.status(200).json({ message: "Bookmark manager API is Running"});
 });
 
+app.get("/bookmark", (request, response)=> {
+    response.status(200).json(bookmarks);
+});
+
 app.listen(port, ()=> {
-    console.log(`Server is running on http://localgost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
